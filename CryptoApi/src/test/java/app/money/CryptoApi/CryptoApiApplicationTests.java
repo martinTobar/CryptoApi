9e8b03209c36d139bc3ffd81ffa388ac7da45a08
Martin Tobar <martoflo99@gmail.com>
2025-01-01 18:21:02 -0500

    First Commit

---
+++ CryptoApi/src/test/java/app/money/CryptoApi/CryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.money.CryptoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
